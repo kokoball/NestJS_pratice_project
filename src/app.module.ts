64c41e17724c48d983527ae230160c487c5e07fd
kokoball <kbp4154@naver.com>
2023-06-23 22:32:04 +0900

    providers 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 // @NOTE: 데코레이터
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [MoviesController],
+  providers: [MoviesService],
 })
 export class AppModule {}
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.movieService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovieId: movieId,
+      ...updateData,
+    };
+  }
+}
